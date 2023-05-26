94c4087f5ab1c799b7b900a9ffd17ad7d72049a3
jeanjoellevillanueva <jeanjoellevillanueva@github.com>
2023-05-26 22:37:44 +0800

    Added a new file for computing the days b4 xmas

---
+++ xmas.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+
+def get_days_before_xmas():
+    """
+    Returns days before xmas.
+    """
+
+    today = datetime.today()
+    xmas_day = 25
+    xmas_month = 12
+    year_now = datetime.today().year
+    xmas_date = f'{year_now}-{xmas_month}-{xmas_day}'
+    # str -> date object: strptime
+    # date object -> str: strftime
+    xmas_day_in_dateobj = datetime.strptime(xmas_date, '%Y-%m-%d')
+    days_before_xmas = (xmas_day_in_dateobj - today).days
+    return days_before_xmas
+
+
+if __name__ == '__main__':
+    get_days_before_xmas()
