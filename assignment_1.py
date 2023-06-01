c8fd5e521cb748b8ba7524d8f8c16b4c2913c0e7
Aaron <ardelacruz1624@gmail.com>
2023-06-01 21:04:32 +0800

    Added docstring to the file

--- assignment_1.py
+++ assignment_1.py
@@ -1,4 +1,5 @@
 class Employee:
+    '''Main Class with empty values for employee details'''
     first_name =''
     last_name = ""
     age = 0
@@ -6,33 +7,40 @@ class Employee:
     gender = ''
 
     def get_position(self):
+        '''Returns the position of the employee'''
         return self.position
 
     def get_fullname(self):
+        '''Returns the fullname of the employee'''
         return f'{self.first_name} {self.last_name}'
 
 
 class Aaron(Employee):
+    '''Employee Aaron details'''
     first_name = "Aaron"
     last_name = "Dela Cruz"
     position = "Tambay"
 
 class Joelle(Employee):
+    '''Employee Joelle details'''
     first_name = "Joelle"
     last_name = "Villanueva"
     position = "Python Developer"
 
 class Miguel(Employee):
+    '''Employee Miguel details'''
     first_name = "Miguel"
     last_name = "Santos"
     position = "Java Developer"
 
 class Joelo(Employee):
+    '''Employee Joelo details'''
     first_name = "Joelo"
     last_name = "Villanueva"
     position = "Valorant Master"
 
 class Edward(Employee):
+    '''Employee Edward details'''
     first_name = "Edward"
     last_name = "Domingo"
     position = "Programmer din siguro"
