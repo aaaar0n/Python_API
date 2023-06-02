54e25a30b67f3434e0b43d3ea15a47d6e983cf8a
Aaron <ardelacruz1624@gmail.com>
2023-06-03 01:12:38 +0800

    Modified added docstring and shorten code

--- api-create-employee.py
+++ api-create-employee.py
@@ -1,42 +1,46 @@
 import requests
-from unidecode import unidecode
 import pdb
 
 class Employee:
+    gender = ""
+    email = ""
     age = 0
     username = ""
-    email = ""
-    password = ""
     phone = ""
-    userdata = ""
+    dob = ""
 
-    def create_user_data(self):
-        data_dict = requests.get('https://randomuser.me/api/').json()
+
+    def create_employee(self):
+        """Returns the full details of a random employee"""
         name = ""
-        login = ""
-        login_list = []
+        user_list = []
+        dob_list = []
+        data_dict = requests.get('https://randomuser.me/api/').json()
         for data in data_dict['results']:
             for name_data in data['name']:
-                initial_name = data['name'][name_data]
-                name += initial_name + " "
-            #pdb.set_trace()
+                """Returns the complete name of employee"""
+                name += data['name'][name_data] + " "
 
-        for data in data_dict['results']:
+            for dob_data in data['dob']:
+                """Returns the date of birth of employee"""
+                initial_dob_data = data['dob'][dob_data]
+                dob_list.append(initial_dob_data)
+            dob = dob_list[0]
+            age = dob_list[1]
+
             for login_data in data['login']:
-                initial_login_info = data['login'][login_data]
-                login_list.append(initial_login_info)
-            username = login_list[1] 
-            password = login_list[2]  
+                """Returns the list of login details of employee"""
+                initial_data = data['login'][login_data]
+                user_list.append(initial_data)
 
-        for data in data_dict['results']:
+            username = user_list[1]
+            password = user_list[2]
             gender = data["gender"]
             email = data['email']
             phone = data['phone']
-        userdata = f"name: {name}\ngender: {gender}\nphone: {phone}\nemail: {email}\nusername: {username}\npassword: {password}"
+        userdata = f"name: {name}\ngender: {gender}\nbirthday: {dob}\nage: {age} \nphone: {phone}\nemail: {email}\nusername: {username}\npassword: {password}"
         return userdata
-            
-class Employee_One(Employee):
-    pass
 
-employee = Employee_One()
-print(employee.create_user_data())
+
+employee = Employee()
+print(employee.create_employee())
